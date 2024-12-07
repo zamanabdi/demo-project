866a3b6183ddb79704e1e386746f234ab4ec0788
zamanabdi <zaman.abdi24@gmail.com>
2024-12-07 15:18:04 +0530

    further updates made

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ function newFeatureAdded(){
     console.log("new feature added...");
 }
 
+function newFeatureAddedAgain(){
+    console.log("new feature added again....");
+}
+
 displayMessage(message);
